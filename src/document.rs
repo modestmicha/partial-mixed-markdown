0e0a92e6849cd200f678bb608afca83f192398d5
Micha <61242418+modestmicha@users.noreply.github.com>
2020-03-11 13:19:59 +0000

    Implement basic parser and writer

--- src/document.rs
+++ src/document.rs
@@ -1,21 +1,96 @@
 use fehler::throws;
+use htmlescape::encode_minimal;
+use lazy_static::lazy_static;
+use pest::{iterators::Pairs, Parser};
+use pest_derive::Parser;
+use regex::Regex;
 use std::io::prelude::*;
+
+// Make sure it's rebuild when grammar changed.
+const _: &[u8] = include_bytes!("grammar.pest");
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct Grammar;
+
+fn escape(string: &str) -> String {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"\s+").unwrap();
+    }
+    encode_minimal(&RE.replace(string.trim(), " ").to_string())
+}
 
 #[derive(Default)]
 pub struct Document {
+    output: Vec<u8>,
 }
 
 impl Document {
     /// Parse a document from string.
     #[throws(_)]
     pub fn parse(input: &str) -> Self {
-        let doc = Self::default();
+        let mut doc = Self::default();
+        let pairs = Grammar::parse(Rule::grammar, &input)?;
+        for pair in pairs {
+            match pair.as_rule() {
+                Rule::header => {
+                    doc.add_header(pair.into_inner())?;
+                }
+                Rule::paragraph => {
+                    doc.add_paragraph(pair.into_inner())?;
+                }
+                Rule::tag => {
+                    doc.add_tag(pair.as_str())?;
+                }
+                _ => panic!("unexpceted token {:#?}", pair),
+            }
+        }
         doc
     }
 
     /// Write the resulting html into the IO stream.
     #[throws(_)]
     pub fn to_writer<W: Write>(&self, writer: &mut W) {
+        writer.write_all(&self.output)?;
+    }
+
+    #[throws(_)]
+    fn add_header<'i>(&mut self, pairs: Pairs<'i, Rule>) {
+        let mut tag = "h1".to_owned();
+        let mut text = String::new();
+        for pair in pairs {
+            let token = pair.as_str();
+            match pair.as_rule() {
+                Rule::atx_header_kind => {
+                    tag = format!("h{}", token.len());
+                }
+                Rule::settext_header_kind => {
+                    if token.chars().next().unwrap() == '-' {
+                        tag = "h2".to_owned();
+                    }
+                }
+                Rule::line => {
+                    text = escape(token);
+                }
+                _ => panic!("unexpceted token {:#?}", pair),
+            }
+        }
+        self.output
+            .write_all(format!("<{}>{}</{}>\n", tag, text, tag).as_bytes())?;
+    }
+
+    #[throws(_)]
+    fn add_paragraph<'i>(&mut self, lines: Pairs<'i, Rule>) {
+        let lines: Vec<String> = lines.map(|line| escape(line.as_str())).collect();
+        let paragraph = lines.join(" ");
+        self.output
+            .write_all(format!("<p>{}</p>\n", paragraph).as_bytes())?;
+    }
+
+    #[throws(_)]
+    fn add_tag<'i>(&mut self, tag: &str) {
+        self.output.write_all(tag.as_bytes())?;
+        self.output.write_all(b"\n")?;
     }
 }
 
